10216254c83a0e6cbd45adabbc107e621d1d3b18
1810Biswa <deyb18860@gamil.com>
2025-01-08 18:58:43 +0530

    refactor: improve formatting in logoutUser function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -156,7 +156,7 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-    await User.findByIdAndUpdate(
+    await User.findByIdAndUpdate(                              
         req.user._id,
         {
             $unset: {
